d184aaad67f952c8f2606f4c30104711633f24f3
buuoltwo <bullnick20@gmail.com>
2025-03-30 18:14:55 +0800

    HttpClient; Jsoup;

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,5 +1,16 @@
 package com.github.hcsp.http;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Crawler {
@@ -19,5 +30,28 @@ public class Crawler {
     }
 
     // 给定一个仓库名，例如"golang/go"，或者"gradle/gradle"，返回第一页的Pull request信息
-    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) {}
+    public static List<GitHubPullRequest> getFirstPageOfPullRequests(String repo) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://github.com/hcsp/read-github-pull-requests/pulls"))
+                .header("Content-Type", "application/json")
+                .GET() // 或者 .POST(HttpRequest.BodyPublishers.ofString("请求体"))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println("Status code: " + response.statusCode());
+//        System.out.println("Response body: " + response.body());
+        String html = response.body();
+        Document doc = Jsoup.parse(html);
+        Element titleEle = doc.selectFirst("#issue_240_link");
+        String title = titleEle != null ?String.valueOf(titleEle.childNodes().get(0)):"";
+        System.out.println(title);
+        List<GitHubPullRequest> result = Arrays.asList(new GitHubPullRequest(240,title,"rwwe"));
+        return result;
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        getFirstPageOfPullRequests("4");
+    }
 }
